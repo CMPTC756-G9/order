eb49faea210e12ae9001e597efa1bdfa9a2e9b66
Nima Modares Gorji <nima.mg@gmail.com>
2023-04-05 20:44:19 -0700

    Set Debug to False

--- order/settings.py
+++ order/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5q=8(qquhw^es-pviu3g)@tk9)vg=fz69@@t7@p+zr%tuntpzk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
